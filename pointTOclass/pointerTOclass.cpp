5b0438932ff8e904d8f4080ac0c372a363720ec5
贝贝 袁 <13007602386@163.com>
2020-02-27 22:12:20 +0800

    pointer

---
+++ pointTOclass/pointerTOclass.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+class Box
+{
+public:
+	Box(double l=2.0,double b=2.0,double h=2.0)
+	{
+		cout << "Constructor is called." << endl;
+		length = l;
+		breadth = b;
+		height = h;
+
+	}
+	double Volume()
+	{
+		return length * breadth*height;
+
+	}
+
+private:
+	double length ;
+	double breadth ;
+	double height ;
+
+};
+
+int main(void)
+{
+	Box Box1(3.0 ,3.0 ,3.0);
+	Box Box2(8.0 ,8.0 ,8.0);
+	Box *ptrBox;
+
+	ptrBox = &Box1;
+	cout << "Volume of Box1: " << ptrBox->Volume() << endl;
+
+	ptrBox = &Box2;
+	cout << "Volume of Box2: " << ptrBox->Volume() << endl;
+
+	return 0;
+}
